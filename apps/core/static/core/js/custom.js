cf8a30f9c0eb3b89a682cfb84c652fab95622e04
Cardionomous Team <roberto.rocuantv@gmail.com>
2020-03-24 02:15:56 -0300

    search profile ready

--- apps/core/static/core/js/custom.js
+++ apps/core/static/core/js/custom.js
@@ -4,13 +4,53 @@ app.config(function ($interpolateProvider) {
     $interpolateProvider.startSymbol('{$');
     $interpolateProvider.endSymbol('$}');
 });
-app.controller("leagueStatsController", function ($scope) {
+app.controller("leagueStatsController", function ($http, $scope) {
     'use strict';
-    $scope.searchSummoner = function ($event) {
-        $event.preventDefault();
-        var data = {
-            nick: $scope.summonerName
-        };
-        console.log(data);
+    $scope.warning = false;
+    $scope.danger = false;
+    $scope.notregistered = false;
+    $scope.template = false;
+    $scope.loading = false;
+    $scope.solo_q_bool = false;
+    $scope.solo_q_bool = false;
+    $scope.searchSummoner = function () {
+        $scope.danger = false;
+        $scope.notregistered = false;
+        $scope.loading = true;
+        if (!$scope.summonerName || !$scope.summonerRegion) {
+            $scope.loading = false;
+            $scope.warning = true;
+        } else {
+            $scope.warning = false;
+            var data = {
+                nick: $scope.summonerName,
+                region: $scope.summonerRegion
+            };
+            $http.post('/profile-api/', data).then(function successCallback(response) {
+                $scope.loading = false;
+                $scope.template = true;
+                $scope.summoner = response.data.summoner;
+                $scope.flex_bool = response.data.flex_bool;
+                if ($scope.flex_bool) {
+                    $scope.flex = response.data.flex;
+                }
+                $scope.solo_q_bool = response.data.solo_q_bool;
+                if ($scope.solo_q_bool) {
+                    $scope.solo_q = response.data.solo_q;
+                }
+                $scope.extra = response.data.extra;
+            }, function errorCallback(response) {
+                if (response.status === 404) {
+                    $scope.loading = false;
+                    $scope.danger = true;
+                } else if (response.status === 400) {
+                    $scope.loading = false;
+                    $scope.notregistered = true;
+                } else {
+                    console.log("Error.");
+                }
+
+            });
+        }
     };
 });
--- apps/core/views.py
+++ apps/core/views.py
@@ -8,6 +8,7 @@ __created__     =       "03/23/2020 02:53"
 from leaguestats.settings import API_KEY
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from math import floor
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import (
@@ -43,17 +44,142 @@ class ProfileAPI(APIView):
 	def post(self, request):
 
 		form = request.data
-
+		
 		nick = form['nick']
+		region = form['region'] # LAN, LAS, NA, EUW.
 
 		try:
-			url = 'https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + str(nick)
+			summoner_url = 'https://' + region + '.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + str(nick)
 			params = {
 				'api_key': API_KEY,
 			}
-			api_call = requests.get(url=url, params=params,).json()
+			summoner_result = requests.get(url=summoner_url, params=params,).json()
 
-			return Response(api_call, status=HTTP_200_OK)
+			summary_url = 'https://' + region + '.api.riotgames.com/lol/league/v4/entries/by-summoner/' + summoner_result['id']
+
+			summary_result = requests.get(url=summary_url, params=params,).json()
+
+			final_summary = {}
+
+			if len(summary_result) == 2:
+
+				# Saving solo_q and flex result.
+				if summary_result[0]['queueType'] == 'RANKED_SOLO_5x5':
+					final_summary['solo_q'] = summary_result[0]
+					final_summary['flex'] = summary_result[1]
+
+				else:
+					final_summary['solo_q'] = summary_result[1]
+					final_summary['flex'] = summary_result[0]
+
+				# Saving solo_q win ratio.
+				if final_summary['solo_q']['wins'] == 0 and final_summary['solo_q']['losses'] == 0:
+					solo_q_wr = 0
+
+				elif final_summary['solo_q']['wins'] > 0 and final_summary['solo_q']['losses'] == 0:
+					solo_q_wr = 100
+
+				else:
+					solo_q_wr = floor(final_summary['solo_q']['wins'] / (final_summary['solo_q']['wins'] + final_summary['solo_q']['losses']) * 100)
+
+				final_summary['solo_q'].update({
+					'solo_q_wr': solo_q_wr,
+					'solo_q_tierIcon': '/media/rank-icon/' + str(final_summary['solo_q']['tier']) + str(final_summary['solo_q']['rank']) + '.png',
+				})
+
+				# Saving flex win ratio.
+				if final_summary['flex']['wins'] == 0 and final_summary['flex']['losses'] == 0:
+					flex_wr = 0
+
+				elif final_summary['flex']['wins'] > 0 and final_summary['flex']['losses'] == 0:
+					flex_wr = 100
+
+				else:
+					flex_wr = floor(final_summary['flex']['wins'] / (final_summary['flex']['wins'] + final_summary['flex']['losses']) * 100)
+
+				final_summary['flex'].update({
+					'flex_wr': flex_wr,
+					'flex_tierIcon': '/media/rank-icon/' + str(final_summary['flex']['tier']) + str(final_summary['flex']['rank']) + '.png',
+				})
+
+				# return Response(final_summary, status=HTTP_200_OK)
+			
+			elif len(summary_result) == 1:
+
+				# Saving solo_q result and win ratio.
+				if summary_result[0]['queueType'] == 'RANKED_SOLO_5x5':
+					final_summary['solo_q'] = summary_result[0]
+
+					if final_summary['solo_q']['wins'] == 0 and final_summary['solo_q']['losses'] == 0:
+						solo_q_wr = 0
+
+					elif final_summary['solo_q']['wins'] > 0 and final_summary['solo_q']['losses'] == 0:
+						solo_q_wr = 100
+
+					else:
+						solo_q_wr = floor(final_summary['solo_q']['wins'] / (final_summary['solo_q']['wins'] + final_summary['solo_q']['losses']) * 100)
+
+					final_summary['solo_q'].update({
+						'solo_q_wr': solo_q_wr,
+						'solo_q_tierIcon': '/media/rank-icon/' + str(final_summary['solo_q']['tier']) + str(final_summary['solo_q']['rank']) + '.png',
+					})
+
+				else:
+
+					# Saving flex result and win ratio.
+					final_summary['flex'] = summary_result[0]	
+
+					if final_summary['flex']['wins'] == 0 and final_summary['flex']['losses'] == 0:
+						flex_wr = 0
+
+					elif final_summary['flex']['wins'] > 0 and final_summary['flex']['losses'] == 0:
+						flex_wr = 100
+
+					else:
+						flex_wr = floor(final_summary['flex']['wins'] / (final_summary['flex']['wins'] + final_summary['flex']['losses']) * 100)
+
+					final_summary['flex'].update({
+						'flex_wr': flex_wr,
+						'flex_tierIcon': '/media/rank-icon/' + str(final_summary['flex']['tier']) + str(final_summary['flex']['rank']) + '.png',
+					})
+
+				# return Response(final_summary, status=HTTP_200_OK)
+
+			else:
+				return Response({'error': 'No data found.'}, status=HTTP_404_NOT_FOUND)
+
+			# Saving profile icon.
+			extra = {
+				'profileIcon': '/media/icon/'+ str(summoner_result['profileIconId']) +'.jpg',
+			}	
+
+			# Final response.
+			final_response = {
+				'summoner': summoner_result,
+				'extra': extra,
+			}
+
+			if 'solo_q' in final_summary:
+				final_response.update({
+					'solo_q': final_summary['solo_q'],
+					'solo_q_bool': True,
+				})
+			else:
+				final_response.update({
+					'solo_q_bool': False,
+				})
+
+			if 'flex' in final_summary:
+				final_response.update({
+					'flex': final_summary['flex'],
+					'flex_bool': True,
+				})
+			else:
+				final_response.update({
+					'flex_bool': False,
+				})
+
+			return Response(final_response, status=HTTP_200_OK)
 
 		except Exception as e:
 			errors = {
@@ -62,8 +188,3 @@ class ProfileAPI(APIView):
 			}
 
 			return Response(errors, status=HTTP_400_BAD_REQUEST)
-
-
-
-
-
